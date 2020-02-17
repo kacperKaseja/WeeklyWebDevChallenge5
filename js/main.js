3b8642e35b1fb3335890bff6b73736ea39bf0236
Kacper Kaseja <yamakaszi10@o2.pl>
2020-02-17 22:22:08 +0100

    asdsad

--- js/main.js
+++ js/main.js
@@ -16,3 +16,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => anchor.addEventListe
         block: 'start'
     });
 }));
+
+function initMap() {
+    // The location of Uluru
+    let uluru = {
+        lat: -25.344,
+        lng: 131.036
+    };
+    // The map, centered at Uluru
+    let map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 5,
+            center: uluru,
+        });
+    // The marker, positioned at Uluru
+    let marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
